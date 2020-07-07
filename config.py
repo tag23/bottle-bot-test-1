149a4ec00413007ee245f87fd8fb36d9edbb5bf5
tag23 <tag1998@mail.ru>
2020-07-07 13:25:16 +0300

    Web.config|YAML changes

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ import os
 class DefaultConfig:
     """ Bot Configuration """
     HOST = os.environ.get("SERVER_HOST", "localhost")
-    PORT = os.environ.get("SERVER_PORT") or os.environ.get("port") or os.environ.get("PORT") or 3979
+    PORT = os.environ.get("SERVER_PORT") or os.environ.get("port") or os.environ.get("PORT") or 3978
     APP_ID = os.environ.get("MicrosoftAppId", "")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", ""),
     CHANNEL_SERVICE = os.environ.get("ChannelService", ""),
